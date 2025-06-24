21650e4de6230eaabe8565490f099012e3d3fe1b
Aarav619 <aaravraj619.ar@gmail.com>
2025-06-24 19:57:12 +0530

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -26,10 +26,6 @@ def favicon():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local
     app.run(host='0.0.0.0', port=port)
