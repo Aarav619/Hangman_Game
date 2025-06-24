c52090fd18d374548a9379860ccfacaec8296099
Aarav619 <aaravraj619.ar@gmail.com>
2025-06-24 21:45:34 +0530

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -5,18 +5,15 @@ import words_file
 
 app = Flask(__name__)
 
-# Home route to render the game
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# API to get a random word
 @app.route('/start', methods=['GET'])
 def start_game():
     word = random.choice(words_file.words)
     return jsonify({"word": word})
 
-# Optional favicon handler (suppresses 404 errors for favicon)
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(
@@ -24,9 +21,5 @@ def favicon():
         'favicon.ico', mimetype='image/vnd.microsoft.icon'
     )
 
-# Run the Flask app
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=True)
